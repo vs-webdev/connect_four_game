c6e636e46d872d0f0ca9fe200f875a1500d84d07
Vikas Solanki <solankivikas066@gmail.com>
2025-12-11 22:30:30 +0530

    feat(timer): implement countdown timer with auto-reset and player toggle

---
+++ src/features/game/components/Timer/Timer.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState, type FC } from 'react'
+import turnBackgroundRed from '../../../../assets/images/turn-background-red.svg'
+import turnBackgroundYellow from '../../../../assets/images/turn-background-yellow.svg'
+import styles from './Timer.module.scss'
+import type { Player } from '../../types/game.types'
+
+interface TimerProps {
+  currentPlayer: Player,
+  togglePlayer: () => void,
+}
+
+const Timer: FC<TimerProps> = ({currentPlayer, togglePlayer}) => {
+  const [timeRemaining, setTimeRemaining] = useState(30)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining(prev => {
+        if (prev === 0) {
+          return 30
+        }
+        return prev - 1;
+      })
+    }, 1000);
+
+    return () => clearInterval(timer)
+  }, [])
+
+  // Toggles Player once timer reaches 0
+  useEffect(() => {
+    if (timeRemaining === 0) togglePlayer()
+  }, [timeRemaining])
+
+  // Reset Timer when currentPlayer changes
+  useEffect(() => {
+    setTimeRemaining(30);
+  }, [currentPlayer]); 
+
+  return (
+    <div className={styles.timerWrapper}>
+      <div className={styles.timerContainer}>
+        <img
+          src={currentPlayer === 'red' ? turnBackgroundRed : turnBackgroundYellow}
+          alt="Turn background"
+          className={styles.timerBg}
+        />
+        <div className={styles.timerContent}>
+          <p className={styles.timerpara}>
+            Player {currentPlayer === 'red' ? '1' : '2'}s turn
+          </p>
+          <span className={styles.timeRemaining}>{timeRemaining}s</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Timer
